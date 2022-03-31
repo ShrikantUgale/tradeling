e3bfd299b761b65b18c15cc474dd7d88a9cde6c0
unknown <user@SPBLRLAP-4850.speridian.com>
2022-03-31 12:39:19 +0400

    hello get post routes added

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express";
+import router from './routes/hello.js';
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    res.status(200).json({"message": "Hello World"});
-})
+//     res.status(200).json({"message": "Hello World"});
+// })
+
+app.use('/hello', router);
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
